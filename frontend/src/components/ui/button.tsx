f2a0ec35b1b74470e52e72f396e074bb74cd2155
Shivam Shukla <110848969+shivamim@users.noreply.github.com>
2026-06-13 23:57:13 +0530

    Update button.tsx

--- frontend/src/components/ui/button.tsx
+++ frontend/src/components/ui/button.tsx
@@ -30,12 +30,15 @@ const buttonVariants = cva(
 
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
-    VariantProps<typeof buttonVariants> {}
+    VariantProps<typeof buttonVariants> {
+  asChild?: boolean
+}
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, variant, size, ...props }, ref) => {
+  ({ className, variant, size, asChild = false, ...props }, ref) => {
+    const Comp = asChild ? React.Slot : 'button'
     return (
-      <button
+      <Comp
         className={cn(buttonVariants({ variant, size, className }))}
         ref={ref}
         {...props}
